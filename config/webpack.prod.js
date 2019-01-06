9553389ef6ad2e50cf4c46c4f0d417c6e6a31e8e
Ruslan Malogulko <ruslan.malogulko@gmail.com>
2019-01-06 23:29:11 +0200

    Remove uglifyjs plugin from deps to avoid heroku confusion

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,7 +4,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: {
