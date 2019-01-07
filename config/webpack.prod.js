3e3ccf58c21b9f3daa94d017a64b1420b4f6f09c
Ruslan Malogulko <ruslan.malogulko@gmail.com>
2019-01-07 05:28:23 +0200

    resolve dependency

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,38 +7,18 @@ const MinifyPlugin = require('babel-minify-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
 const BrotliWebpackPlugin = require('brotli-webpack-plugin');
 
+const commonConfig = require('./webpack.common');
+
 module.exports = {
+    ...commonConfig,
     entry: {
         main: [
             "./src/main.js"
         ]
     },
     mode: "production",
-    output: {
-        filename: "[name]-bundle.js",
-        path: path.resolve(__dirname, "../dist"),
-    },
-    // devtool: 'source-map',
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: "babel-loader"
-                },
-                exclude: /node_modules/
-            },
-             {
-                test: /\.md$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    },
-                    {
-                        loader: "markdown-loader"
-                    }
-                ]
-            },
             {
                 test: /\.styl$/,
                 use: [
@@ -48,28 +28,7 @@ module.exports = {
                     { loader: "stylus-loader" }
                 ]
             },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: {
-                            attrs: ["img:src"]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.jpg$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "images/[name].[ext]"
-                        }
-                    }
-                ]
-            }
+            ...commonConfig.module.rules
         ]
     },
     plugins: [
